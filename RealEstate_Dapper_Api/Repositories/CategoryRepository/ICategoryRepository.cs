85e475fdebc4929dbd586ada62ad32d5753c5329
Çağrı <ar@DESKTOP-R1KI4H5>
2024-05-14 02:08:48 +0300

    Add project files.

---
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using RealEstate_Dapper_Api.DTOs.CategoryDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
+{
+    public interface ICategoryRepository
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+    }
+}

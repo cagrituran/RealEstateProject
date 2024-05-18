5ae249a0fc9305cb01a9f939caa041c918c9d763
cagrituran <cagri.turann@hotmail.com>
2024-05-18 03:13:34 +0300

    #9 Session

---
+++ RealEstate_Dapper_Api/DTOs/ProductDtos/ResultProductWithCategoryDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RealEstate_Dapper_Api.DTOs.ProductDtos
+{
+    public class ResultProductWithCategoryDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/ProductRepository/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿
+
+using Dapper;
+using RealEstate_Dapper_Api.DTOs.CategoryDtos;
+using RealEstate_Dapper_Api.DTOs.ProductDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.ProductRepository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly Context _context;
+
+        public ProductRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResultProductDto>> GetAllProductAsync()
+        {
+            string query = "Select * From Product";
+
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultProductDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<List<ResultProductWithCategoryDto>> GetAllProductWithCategoryAsync()
+        {
+            string query = "Select p.Id,p.Title,p.Price,p.City,p.District,c.Name From Product as p inner join Category as c on p.ProductCategory = c.Id";
+
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultProductWithCategoryDto>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
